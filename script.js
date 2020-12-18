8e573ddc25961058076b8e414d4daf9fed004797
Anna Moignard <amoignard.coder@gmail.com>
2020-12-18 13:46:44 +1100

    working to add a colour randomizer to the increment and decrement buttons

--- script.js
+++ script.js
@@ -2,23 +2,38 @@ let counterDisplay = document.querySelector('.counter-display');
 let counterMinus = document.querySelector('.minus-one');
 let counterPlus = document.querySelector('.plus-one');
 let count = 0;
+let randomColor = 0;
 
 updateDisplay();
-// clicks.addEventListener("click", () => {
-//   clicks += 1; 
-//   document.querySelector("clicks").innerHTML = clicks;
-// })
 
-counterMinus.addEventListener("click",() => {
+counterMinus.addEventListener("click", () => {
   count -= 1;
   updateDisplay();
 });
 
-counterPlus.addEventListener("click",() => {
+counterPlus.addEventListener("click", () => {
   count += 1;
   updateDisplay();
 });
 
-function updateDisplay(){
+function updateDisplay() {
   counterDisplay.innerHTML = count;
 };
+
+function randomize() {
+  document.getElementById('random-color').style.color = randomColor;
+}; 
+
+randomColor.addEventListener('Click', () => {
+  return 'hsl(' + (360 * Math.random()) + ',50%,50%)';
+  //   return '#' + (0x1000000 + (Math.random()) * 0xffffff).toString(16).substr(1, 6);
+});
+
+// function randomColor() {
+//   return 'hsl(' + (360 * Math.random()) + ',50%,50%)';
+// };
+
+
+// counterPlus.addEventListener('onClick', randomColor => {
+//   document.querySelector(".counter-display").style.color = newColor;
+// }
