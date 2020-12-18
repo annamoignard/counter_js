a80e65f4a209840945f066e7d551a10be66fabd6
Anna Moignard <amoignard.coder@gmail.com>
2020-12-18 15:03:30 +1100

    color randomly changes when either incrementer or decrementer is selected

--- script.js
+++ script.js
@@ -2,38 +2,27 @@ let counterDisplay = document.querySelector('.counter-display');
 let counterMinus = document.querySelector('.minus-one');
 let counterPlus = document.querySelector('.plus-one');
 let count = 0;
-let randomColor = 0;
 
 updateDisplay();
 
 counterMinus.addEventListener("click", () => {
   count -= 1;
   updateDisplay();
+  document.getElementById('random-color').style.color = randomColor();
 });
 
 counterPlus.addEventListener("click", () => {
   count += 1;
   updateDisplay();
+  document.getElementById('random-color').style.color = randomColor();
 });
 
 function updateDisplay() {
   counterDisplay.innerHTML = count;
 };
 
-function randomize() {
-  document.getElementById('random-color').style.color = randomColor;
+function randomColor() {
+  return '#' + (0x1000000 + (Math.random()) * 0xffffff).toString(16).substr(1, 6);
 }; 
 
-randomColor.addEventListener('Click', () => {
-  return 'hsl(' + (360 * Math.random()) + ',50%,50%)';
-  //   return '#' + (0x1000000 + (Math.random()) * 0xffffff).toString(16).substr(1, 6);
-});
 
-// function randomColor() {
-//   return 'hsl(' + (360 * Math.random()) + ',50%,50%)';
-// };
-
-
-// counterPlus.addEventListener('onClick', randomColor => {
-//   document.querySelector(".counter-display").style.color = newColor;
-// }
